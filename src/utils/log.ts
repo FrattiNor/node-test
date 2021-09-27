e86e2974090e1cba85c447c4f093e421e7ce1d92
sunzhibin <fn157150@163.com>
2021-09-27 11:39:37 +0800

    [fix] log4js config

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -2,8 +2,8 @@ import { configure, getLogger } from 'log4js'
 import * as path from 'path'
 
 configure({
-    appenders: { cheese: { type: 'file', filename: path.join(__dirname, '../../log/cheese') } },
-    categories: { default: { appenders: ['cheese'], level: 'trace' } }
+    appenders: { cheese: { type: 'file', filename: path.join(__dirname, '../../log/cheese.log') } }, // 定义日志文件
+    categories: { default: { appenders: ['cheese'], level: 'trace' } } // 定义日志级别
 })
 
 const logger = getLogger()
